d667cfa53e137982e31ac95712801c79d15e2773
shashi <shashi4a2001@gmail.com>
2020-10-30 17:15:59 +0530

    commit...

---
+++ DBScript/dbo.usp_SelectClientTypeMaster.StoredProcedure.sql
@@ -0,0 +1,18 @@
+
+if Exists(Select 1 from sys.procedures where name='usp_SelectClientTypeMaster')
+Begin	
+	Drop Procedure usp_SelectClientTypeMaster
+End
+GO
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+Create Procedure usp_SelectClientTypeMaster
+As
+Begin
+	Select ClientTypeLevel,ClientTypeCode,ClientTypeName,
+	CanMakeSuperAdmin,CanMakeMasterAgent,CanMakeSubAgent,
+	CanMakeFranchisee,CanMakeFranchiseeAgent,CanMakeIndividual 
+	From ClientTypeMaster With(NoLock)	
+End
