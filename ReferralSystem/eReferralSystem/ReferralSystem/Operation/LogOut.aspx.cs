dbcccc7f3db4d69a09003438d9c7d37e3aceae67
shashi <shashi4a2001@gmail.com>
2020-10-30 18:38:56 +0530

    commit..

--- ReferralSystem/eReferralSystem/ReferralSystem/Operation/LogOut.aspx.cs
+++ ReferralSystem/eReferralSystem/ReferralSystem/Operation/LogOut.aspx.cs
@@ -19,89 +19,10 @@ public partial class Operation_LogOut : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        //base.OnLoad(e);
-        clsErrorLogger objclsErrorLogger = new clsErrorLogger();
-        clsCommon objclsCommon = new clsCommon();
-        //STPclsLogin objSTPclsLogin = new STPclsLogin();
-        string _strAlertMessage = string.Empty;
-        DataTable dtInfo;
-        try
-        {
-
-
-            //Validate User Authetication Starts
-            if (Context.User.Identity.IsAuthenticated)
-            {
-                //Check User Session and Connection String 
-                if (Session["PortalUserDtl"] != null)
-                {
-                    user = (ObjPortalUser)Session["PortalUserDtl"];
-
-                    //Check Browser Address
-                    if (objclsCommon.GetIPAddress() != user.IPAddress)
-                    {
-                        Session.Clear();
-                        Session.RemoveAll();
-                        Session.Abandon();
-                        FormsAuthentication.SignOut();
-                        Response.Redirect("login.aspx?Exp=Y", false);
-                    }
-                }
-                else
-                {
-                    Session.Clear();
-                    Session.RemoveAll();
-                    Session.Abandon();
-                    FormsAuthentication.SignOut();
-                    Response.Redirect("login.aspx?Exp=Y", false);
-                    return;
-                }
-            }
-            else
-            {
-                Session.Clear();
-                Session.RemoveAll();
-                Session.Abandon();
-                FormsAuthentication.SignOut();
-                Response.Redirect("login.aspx?Exp=Y", false);
-                return;
-            }
-            //Validate User Authetication Ends
-            //Check User Session and Connection String 
-
-            if (IsPostBack == false)
-            {
-                //dtInfo = objSTPclsLogin.LogOut(user.EmailId, user.LogId, user.ConnectionString);
-                _strMsgType = "Success";
-                _strAlertMessage = "You have successfully LoggedOut.";
-
-                Session.Clear();
-                Session.RemoveAll();
-                Session.Abandon();
-                FormsAuthentication.SignOut();
-            }
-        }
-        catch (Exception ex)
-        {
-            _strMsgType = "Error";
-            _strAlertMessage = ex.Message;
-            objclsErrorLogger.WriteError(System.IO.Path.GetFileName(Request.Path), "", user == null ? "" : user.ConnectionString, ex);
-            Response.Redirect("~/Operation/Err.aspx");
-        }
-        finally
-        {
-            //Alert Message
-            if (_strAlertMessage != string.Empty && _strAlertMessage.Trim() != "")
-            {
-                lblMessage.Text = _strAlertMessage;
-                objclsCommon.setLabelMessage(lblMessage, _strMsgType);
-                ScriptManager.RegisterStartupScript(this, typeof(Page), "AutoHide", "<script>HideLabel();</script>", false);
-                ScriptManager.RegisterStartupScript(this, typeof(Page), "Alert", "<script>alert('" + lblMessage.Text + "');</script>", false);
-            }
-            objclsErrorLogger = null;
-            objclsCommon = null;
-            //objSTPclsLogin = null;
-            dtInfo = null;
-        }
+        lblMessage.Text = "You have successfully Logged Out.";
+        Session.Clear();
+        Session.RemoveAll();
+        Session.Abandon();
+        FormsAuthentication.SignOut();
     }
 }
