549e005f879d10c8e6b729611d742807beb57b97
shashi <shashi4a2001@gmail.com>
2020-10-31 14:06:04 +0530

    commit..

---
+++ DBScript/dbo.usp_InsertClientMaster.StoredProcedure.sql
@@ -0,0 +1,47 @@
+
+if Exists(Select 1 from sys.procedures where name='usp_InsertClientMaster')
+Begin	
+	Drop Procedure usp_InsertClientMaster
+End
+GO
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+Create Procedure usp_InsertClientMaster
+@MobileNo varchar(20),
+@ClientTypeCode varchar(10),
+@ClientRefId BigInt =Null,
+@ClientCode varchar(100),
+@ClientName varchar(200),
+@ContactPerson varchar(200),
+@EmailId varchar(100),
+@LandlineNo varchar(100),
+@Address varchar(500),
+@LoginId varchar(100),
+@LoginPassword varchar(100),
+@BankName varchar(200),
+@ClientNameAsPerBank varchar(300),
+@AccountNo varchar(100),
+@IFSCCode varchar(100),
+@SharingPercentage Numeric(18,2),
+@SelfReferralCode varchar(50),
+@ReferredReferralCode varchar(50),
+@UserId varchar(100)
+As
+Begin
+	Declare @ReferralAmount Numeric(18,2)
+	Declare @ReferredReferralRevenue Numeric(18,2)
+
+	Insert Into ClientMaster(
+	MobileNo,ClientRefId,ClientCode,ClientName,ClientTypeCode,ContactPerson,EmailId,
+	LandlineNo,Address,LoginId,LoginPassword,BankName,ClientNameAsPerBank,AccountNo,
+	IFSCCode,SharingPercentage,SelfReferralCode,ReferredReferralCode,ReferralAmount,
+	ReferredReferralRevenue,CreatedBy,CreatedDate)
+	values(
+	@MobileNo,@ClientRefId,@ClientCode,@ClientName,@ClientTypeCode,@ContactPerson,@EmailId,
+	@LandlineNo,@Address,@LoginId,@LoginPassword,@BankName,@ClientNameAsPerBank,@AccountNo,
+	@IFSCCode,@SharingPercentage,@SelfReferralCode,@ReferredReferralCode,@ReferralAmount,
+	@ReferredReferralRevenue,@UserId,getdate())
+
+End
---
+++ DBScript/dbo.usp_SelectClientTypePermissionList.StoredProcedure.sql
@@ -0,0 +1,49 @@
+
+if Exists(Select 1 from sys.procedures where name='usp_SelectClientTypePermissionList')
+Begin	
+	Drop Procedure usp_SelectClientTypePermissionList
+End
+GO
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+Create Procedure usp_SelectClientTypePermissionList
+@ClientTypeCode varchar(50)
+As
+Begin
+	 
+	Select * Into #t1 From ClientTypeMaster With(NoLock)	
+	Where ClientTypeCode = @ClientTypeCode
+
+	Select ClientTypeLevel,ClientTypeCode,ClientTypeName 
+	Into #t2
+	From ClientTypeMaster With(NoLock)	
+	
+	If Not Exists (Select 1 From #t1 Where CanMakeSuperAdmin =1)
+	Begin
+		Delete From #t2 Where ClientTypeCode='100'
+	End
+	If Not Exists (Select 1 From #t1 Where CanMakeMasterAgent =1)
+	Begin
+		Delete From #t2 Where ClientTypeCode='101'
+	End
+	If Not Exists (Select 1 From #t1 Where CanMakeSubAgent =1)
+	Begin
+		Delete From #t2 Where ClientTypeCode='102'
+	End
+	If Not Exists (Select 1 From #t1 Where CanMakeFranchisee =1)
+	Begin
+		Delete From #t2 Where ClientTypeCode='103'
+	End
+	If Not Exists (Select 1 From #t1 Where CanMakeFranchiseeAgent =1)
+	Begin
+		Delete From #t2 Where ClientTypeCode='104'
+	End
+	If Not Exists (Select 1 From #t1 Where CanMakeIndividual =1)
+	Begin
+		Delete From #t2 Where ClientTypeCode='105'
+	End
+
+	Select * From #t2 Order By ClientTypeLevel 
+End
