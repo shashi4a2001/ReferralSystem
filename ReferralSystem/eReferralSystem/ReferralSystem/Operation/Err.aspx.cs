c700916a7653b5e9c685e7e6e1691e0d37ed8a97
shashi <shashi4a2001@gmail.com>
2020-12-15 23:39:40 +0530

    commit..

--- ReferralSystem/eReferralSystem/ReferralSystem/Operation/Err.aspx.cs
+++ ReferralSystem/eReferralSystem/ReferralSystem/Operation/Err.aspx.cs
@@ -11,64 +11,17 @@ using BLCMR;
     public partial class Operation_Err : System.Web.UI.Page
     {
         ObjPortalUser user;
- 
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            //base.OnLoad(e);
-            clsErrorLogger objclsErrorLogger = new clsErrorLogger();
-            clsCommon objclsCommon = new clsCommon();
-             
-            string _strAlertMessage = string.Empty;
-            DataTable  dtInfo;
-            try
-            {
 
-                lblWorkLocation.Text = objclsCommon.DecryptData(ConfigurationManager.AppSettings["WorkLocation"].ToString());
-                lblOrganization.Text = objclsCommon.DecryptData(ConfigurationManager.AppSettings["Organization"].ToString());
-                lblPhone.Text = objclsCommon.DecryptData(ConfigurationManager.AppSettings["Phone"].ToString());
-                lblEmail.Text = objclsCommon.DecryptData(ConfigurationManager.AppSettings["Email"].ToString());
-                Page.Title = "@All Right Reserved." + lblOrganization.Text;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblMessage.Text = "We are very sorry for the inconvenience caused to you...";
+        Session.Clear();
+        Session.RemoveAll();
+        Session.Abandon();
+        System.Web.Security.FormsAuthentication.SignOut();
 
-                //Validate User Authetication Starts
-                if (Context.User.Identity.IsAuthenticated)
-                {
-                    //Check User Session and Connection String 
-                    if (Session["PortalUserDtl"] != null)
-                    {
-                        user = (ObjPortalUser)Session["PortalUserDtl"];
-                    }
-                }
-
-                //Clear Session
-                Session.Clear();
-                Session.Contents.Clear();
-                Session.RemoveAll();
-                Session.Abandon();
-                //Validate User Authetication Ends
-
-                if (IsPostBack == false)
-                {
-                    //if (user != null)
-                    //{
-                    //    dtInfo = objSTPclsLogin.LogOut(user.EmailId, user.LogId, user.ConnectionString);
-                    //}
-                    lblMessage.Text = " <b>We are very sorry for the inconvenience caused to you...<br> </b>";
-                }
-
-            }
-            catch (Exception ex)
-            {
-                _strAlertMessage = ex.Message;
-                objclsErrorLogger.WriteError(System.IO.Path.GetFileName(Request.Path), "", user==null?"" : user.ConnectionString,   ex);
-            }
-            finally
-            {
-                objclsErrorLogger = null;
-                objclsCommon = null;
-                //objSTPclsLogin = null;
-                dtInfo = null;
-            }
-        }
+        return;
+    }
     }
  
  
