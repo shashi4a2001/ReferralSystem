bdfeb76d088f07cf089405f5a5a0e102841130bd
shashi <shashi4a2001@gmail.com>
2020-12-16 15:43:40 +0530

    commit..

--- DBScript/dbo.usp_AccountOpenReport.StoredProcedure.sql
+++ DBScript/dbo.usp_AccountOpenReport.StoredProcedure.sql
@@ -30,30 +30,225 @@ Begin
 	
 	If @ReportType='Detail'
 	Begin	
-		Select 
-		a.ClientId,a.ClientCode,a.ClientName,b.ClientTypeName As [ClientType],a.ContactPerson,a.EmailId,
-		a.MobileNo, a.ReferralSharingPercentage As [Referral Sharing Percentage],a.ReferralAmount As [Referral Amount],
-		a.ReferredReferralRevenue As [Referred Referral Revenue], 
-		CONVERT(varchar,a.CreatedDate,9) As [Account Opening Date]  
-		From ClientMaster a With(NoLock)
-		Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
-		Where 
-		ReferredReferralCode=@SelfReferralCode 
-		and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
-		and a.CreatedDate Between @DateFrom And @DateTo
+
+		--If @ClientTypeCode='100'--Super Admin
+		--Begin
+		--End
+		--Else If @ClientTypeCode='101'--National Head
+		--Begin
+		--End
+		--Else If @ClientTypeCode='102'--Regional Head
+		--Begin
+		--End
+		--Else If @ClientTypeCode='103'--State Franchisee
+		--Begin
+		--End
+		--Else If @ClientTypeCode='104'--District Franchisee
+		--Begin
+		--End
+		--Else If @ClientTypeCode='105'--Individual Agent
+		--Begin
+		--End
+		--Else If @ClientTypeCode='106'--Individual	
+		--Begin
+		--End
+
+		If @ClientTypeCode='100'--Super Admin
+		Begin
+			Select 
+			a.ClientId,a.ClientCode,a.ClientName,b.ClientTypeName As [ClientType],a.ContactPerson,a.EmailId,
+			a.MobileNo, a.ReferralSharingPercentage As [Referral Sharing Percentage],a.ReferralAmount As [Referral Amount],
+			a.ReferredReferralRevenue As [Referred Referral Revenue], 
+			CONVERT(varchar,a.CreatedDate,9) As [Account Opening Date]  
+			From ClientMaster a With(NoLock)
+			Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
+			Where 
+			a.ClientTypeCode <>'100'
+			and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
+			and a.CreatedDate Between @DateFrom And @DateTo
+		End
+		Else If @ClientTypeCode='101'--National Head
+		Begin
+			Select 
+			a.ClientId,a.ClientCode,a.ClientName,b.ClientTypeName As [ClientType],a.ContactPerson,a.EmailId,
+			a.MobileNo, a.ReferralSharingPercentage As [Referral Sharing Percentage],a.ReferralAmount As [Referral Amount],
+			a.ReferredReferralRevenue As [Referred Referral Revenue], 
+			CONVERT(varchar,a.CreatedDate,9) As [Account Opening Date]  
+			From ClientMaster a With(NoLock)
+			Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
+			Where 
+			a.ClientTypeCode In  ('102','103','104','105','106')
+			and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
+			and a.CreatedDate Between @DateFrom And @DateTo
+		End
+		Else If @ClientTypeCode='102'--Regional Head
+		Begin
+			Select 
+			a.ClientId,a.ClientCode,a.ClientName,b.ClientTypeName As [ClientType],a.ContactPerson,a.EmailId,
+			a.MobileNo, a.ReferralSharingPercentage As [Referral Sharing Percentage],a.ReferralAmount As [Referral Amount],
+			a.ReferredReferralRevenue As [Referred Referral Revenue], 
+			CONVERT(varchar,a.CreatedDate,9) As [Account Opening Date]  
+			From ClientMaster a With(NoLock)
+			Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
+			Where 
+			a.ClientTypeCode In  ('103','104','105','106')
+			and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
+			and a.CreatedDate Between @DateFrom And @DateTo
+		End
+		Else If @ClientTypeCode='103'--State Franchisee
+		Begin
+			Select 
+			a.ClientId,a.ClientCode,a.ClientName,b.ClientTypeName As [ClientType],a.ContactPerson,a.EmailId,
+			a.MobileNo, a.ReferralSharingPercentage As [Referral Sharing Percentage],a.ReferralAmount As [Referral Amount],
+			a.ReferredReferralRevenue As [Referred Referral Revenue], 
+			CONVERT(varchar,a.CreatedDate,9) As [Account Opening Date]  
+			From ClientMaster a With(NoLock)
+			Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
+			Where 
+			a.ClientTypeCode In  ('104','105','106')
+			and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
+			and a.CreatedDate Between @DateFrom And @DateTo
+		End
+		Else If @ClientTypeCode='104'--District Franchisee
+		Begin
+			Select 
+			a.ClientId,a.ClientCode,a.ClientName,b.ClientTypeName As [ClientType],a.ContactPerson,a.EmailId,
+			a.MobileNo, a.ReferralSharingPercentage As [Referral Sharing Percentage],a.ReferralAmount As [Referral Amount],
+			a.ReferredReferralRevenue As [Referred Referral Revenue], 
+			CONVERT(varchar,a.CreatedDate,9) As [Account Opening Date]  
+			From ClientMaster a With(NoLock)
+			Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
+			Where 
+			a.ClientTypeCode In  ('105','106')
+			and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
+			and a.CreatedDate Between @DateFrom And @DateTo
+
+		End
+		Else If @ClientTypeCode='105'--Individual Agent
+		Begin
+			Select 
+			a.ClientId,a.ClientCode,a.ClientName,b.ClientTypeName As [ClientType],a.ContactPerson,a.EmailId,
+			a.MobileNo, a.ReferralSharingPercentage As [Referral Sharing Percentage],a.ReferralAmount As [Referral Amount],
+			a.ReferredReferralRevenue As [Referred Referral Revenue], 
+			CONVERT(varchar,a.CreatedDate,9) As [Account Opening Date]  
+			From ClientMaster a With(NoLock)
+			Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
+			Where 
+			ReferredReferralCode=@SelfReferralCode 
+			and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
+			and a.CreatedDate Between @DateFrom And @DateTo
+		End
+		Else If @ClientTypeCode='106'--Individual	
+		Begin
+			Select 
+			a.ClientId,a.ClientCode,a.ClientName,b.ClientTypeName As [ClientType],a.ContactPerson,a.EmailId,
+			a.MobileNo, a.ReferralSharingPercentage As [Referral Sharing Percentage],a.ReferralAmount As [Referral Amount],
+			a.ReferredReferralRevenue As [Referred Referral Revenue], 
+			CONVERT(varchar,a.CreatedDate,9) As [Account Opening Date]  
+			From ClientMaster a With(NoLock)
+			Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
+			Where 
+			ReferredReferralCode=@SelfReferralCode 
+			and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
+			and a.CreatedDate Between @DateFrom And @DateTo
+		End
+
+		
 	End
 	If @ReportType='Summary'
 	Begin	
-		Select 
-		b.ClientTypeName As [ClientType],Count(1) As [No .Of Account],
-		Sum(a.ReferralAmount) As [Referral Amount],Sum(a.ReferredReferralRevenue) As [Referred Referral Revenue]
-		From ClientMaster a With(NoLock)
-		Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
-		Where 
-		ReferredReferralCode=@SelfReferralCode 
-		and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
-		and a.CreatedDate Between @DateFrom And @DateTo
-		Group By b.ClientTypeName
+		If @ClientTypeCode='100'--Super Admin
+		Begin
+			Select 
+			b.ClientTypeName As [ClientType],Count(1) As [No .Of Account],
+			Sum(a.ReferralAmount) As [Referral Amount],Sum(a.ReferredReferralRevenue) As [Referred Referral Revenue]
+			From ClientMaster a With(NoLock)
+			Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
+			Where 
+			a.ClientTypeCode <>'100'
+			and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
+			and a.CreatedDate Between @DateFrom And @DateTo
+			Group By b.ClientTypeName
+		End
+		Else If @ClientTypeCode='101'--National Head
+		Begin
+			Select 
+			b.ClientTypeName As [ClientType],Count(1) As [No .Of Account],
+			Sum(a.ReferralAmount) As [Referral Amount],Sum(a.ReferredReferralRevenue) As [Referred Referral Revenue]
+			From ClientMaster a With(NoLock)
+			Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
+			Where 
+			a.ClientTypeCode In  ('102','103','104','105','106')
+			and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
+			and a.CreatedDate Between @DateFrom And @DateTo
+			Group By b.ClientTypeName
+		End
+		Else If @ClientTypeCode='102'--Regional Head
+		Begin
+			Select 
+			b.ClientTypeName As [ClientType],Count(1) As [No .Of Account],
+			Sum(a.ReferralAmount) As [Referral Amount],Sum(a.ReferredReferralRevenue) As [Referred Referral Revenue]
+			From ClientMaster a With(NoLock)
+			Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
+			Where 
+			a.ClientTypeCode In  ('103','104','105','106')
+			and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
+			and a.CreatedDate Between @DateFrom And @DateTo
+			Group By b.ClientTypeName
+		End
+		Else If @ClientTypeCode='103'--State Franchisee
+		Begin
+			Select 
+			b.ClientTypeName As [ClientType],Count(1) As [No .Of Account],
+			Sum(a.ReferralAmount) As [Referral Amount],Sum(a.ReferredReferralRevenue) As [Referred Referral Revenue]
+			From ClientMaster a With(NoLock)
+			Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
+			Where 
+			a.ClientTypeCode In  ('104','105','106')
+			and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
+			and a.CreatedDate Between @DateFrom And @DateTo
+			Group By b.ClientTypeName
+		End
+		Else If @ClientTypeCode='104'--District Franchisee
+		Begin
+			Select 
+			b.ClientTypeName As [ClientType],Count(1) As [No .Of Account],
+			Sum(a.ReferralAmount) As [Referral Amount],Sum(a.ReferredReferralRevenue) As [Referred Referral Revenue]
+			From ClientMaster a With(NoLock)
+			Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
+			Where 
+			a.ClientTypeCode In  ('105','106')
+			and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
+			and a.CreatedDate Between @DateFrom And @DateTo
+			Group By b.ClientTypeName
+		End
+		Else If @ClientTypeCode='105'--Individual Agent
+		Begin
+			Select 
+			b.ClientTypeName As [ClientType],Count(1) As [No .Of Account],
+			Sum(a.ReferralAmount) As [Referral Amount],Sum(a.ReferredReferralRevenue) As [Referred Referral Revenue]
+			From ClientMaster a With(NoLock)
+			Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
+			Where 
+			ReferredReferralCode=@SelfReferralCode 
+			and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
+			and a.CreatedDate Between @DateFrom And @DateTo
+			Group By b.ClientTypeName
+		End
+		Else If @ClientTypeCode='106'--Individual	
+		Begin
+			Select 
+			b.ClientTypeName As [ClientType],Count(1) As [No .Of Account],
+			Sum(a.ReferralAmount) As [Referral Amount],Sum(a.ReferredReferralRevenue) As [Referred Referral Revenue]
+			From ClientMaster a With(NoLock)
+			Inner Join ClientTypeMaster b With(NoLock) ON a.ClientTypeCode = b.ClientTypeCode  
+			Where 
+			ReferredReferralCode=@SelfReferralCode 
+			and a.ClientTypeCode =IsNull(@ClientTypeCode,a.ClientTypeCode)
+			and a.CreatedDate Between @DateFrom And @DateTo
+			Group By b.ClientTypeName
+		End
+		
 	End
 
 End
