452c52e516b224ddf13dd81e96ac348b20fa4915
shashi <shashi4a2001@gmail.com>
2021-02-21 04:58:00 +0530

    commit...

--- DBScript/dbo.usp_CalculateBrokerage.StoredProcedure.sql
+++ DBScript/dbo.usp_CalculateBrokerage.StoredProcedure.sql
@@ -10,12 +10,29 @@ SET QUOTED_IDENTIFIER ON
 GO
 --Exec usp_AccountOpenReport '101'
 Create Procedure usp_CalculateBrokerage
-@ParentClientId BigInt = Null,
-@ClientId BigInt = Null,
 @DateFrom DateTime = Null,
-@DateTo DateTime = Null
+@DateTo DateTime = Null,
+@BrokerageType varchar(50), --Referral/Revenue
+@BrokerageAmount Numeric(18,2),
+@UserId varchar(50)
 As
 Begin
  
+	 If Exists (
+				Select 1 From BrokerageSummary 
+				Where BrokerageType=@BrokerageType 
+				And ( (DateFrom Between @DateFrom And @DateTo) Or (DateTo Between @DateFrom And @DateTo) )
+				)
+	 Begin
+		Select 'Brokerage already calculated for this period'
+		Return
+	 End
+
+	 Declare @Id BigInt
+	 Insert Into BrokerageSummary(DateFrom,DateTo,BrokerageType,BrokerageAmount,CreatedBy,CreatedDate)
+						   values(@DateFrom,@DateTo,@BrokerageType,@BrokerageAmount,@UserId,getDate())
+
+	Set @Id=@@IDENTITY
+
 	
 End
