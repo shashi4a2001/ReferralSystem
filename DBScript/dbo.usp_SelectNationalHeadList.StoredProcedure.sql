2f51c8853826976229fe0090d908999a365139f7
shashi <shashi4a2001@gmail.com>
2021-02-23 08:57:51 +0530

    commit...

--- DBScript/dbo.usp_SelectNationalHeadList.StoredProcedure.sql
+++ DBScript/dbo.usp_SelectNationalHeadList.StoredProcedure.sql
@@ -13,5 +13,10 @@ Create Procedure usp_SelectNationalHeadList
  
 As
 Begin
+Select ClientId,ClientName From
+	(
+	Select 0 As [ClientId],'--Select--' As [ClientName]
+	Union All
 	Select ClientId,ClientName From ClientMaster Where ClientTypeCode=101
+	) a Order By a.ClientId
 End
